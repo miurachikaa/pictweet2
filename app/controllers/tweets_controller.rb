1b85ab053091b6aad2dffe1a694ef4f40df0d9df
miurachikaa <chikaa719@gmail.com>
2023-02-12 17:37:08 +0900

    ツイートの編集

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -16,7 +16,16 @@ class TweetsController < ApplicationController
     tweet = Tweet.find(params[:id])
     tweet.destroy
   end
-  
+
+  def edit
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+  end
+
   private
   def tweet_params
     params.require(:tweet).permit(:name, :image, :text)
